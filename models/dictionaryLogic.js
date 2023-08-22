760fc011847fb31829f23c4b02f403df3eae3b48
D20CM <scottbrown500@hotmail.com>
2023-08-22 13:32:18 +0100

    move logic to models folder

---
+++ models/dictionaryLogic.js
@@ -0,0 +1,7 @@
+//test that query parameters can be passed back and forth
+export async function handleLetters(letters) {
+  let lettersArray = letters.split("");
+  return lettersArray;
+}
+
+//todo: add the game logic here (or import to here) and initiate the api calls where needed
